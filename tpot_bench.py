7fe3eaa28977db50b16ecbe2fc35305c9f8c120b
Sushi <susheendharvj@gmail.com>
2020-01-16 13:45:47 +0000

    added prediction time calculation for tpot

--- tpot_bench.py
+++ tpot_bench.py
@@ -12,6 +12,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction import DictVectorizer
 warnings.simplefilter(action="ignore", category=DeprecationWarning)
 import random
+import time
 
 
 
@@ -48,16 +49,21 @@ def tpot_benchmarking(data_id,problem_type):
 		df['target'] = le.fit_transform(df['target'])
 		training_indices,testing_indices = training_indices,validation_indices = train_test_split(df.index,stratify=y,train_size=0.75,test_size=0.25)
 		f1_score = []
+		time_to_predict =[]
 		
 
 
 		for seed in random.sample(range(1,100),2):
+
 			tpot = TPOTClassifier(verbosity=2,max_eval_time_mins=0.04,max_time_mins=1,population_size=15,cv=5,random_state=seed,scoring='f1')
 			tpot.fit(df.drop('target',axis=1).loc[training_indices],df.loc[training_indices,'target'])
-			f1_score.append(tpot.score(df.drop('target',axis=1).loc[validation_indices].values, df.loc[validation_indices, 'target'].values))
+			start_time = time.time()
+			preds = tpot.score(df.drop('target',axis=1).loc[validation_indices].values,df.loc[validation_indices, 'target'].values)
+			time_to_predict.append(time.time() - start_time)
+			f1_score.append(preds)
 
 
-		return np.mean(f1_score)
+		return np.mean(f1_score),np.mean(time_to_predict)
 
 	if problem_type == 'regression':
 		X,y,categorical_indicator,attribute_names = data.get_data(target=data.default_target_attribute)
@@ -69,23 +75,25 @@ def tpot_benchmarking(data_id,problem_type):
 		df['target'] = y
 		training_indices,testing_indices = training_indices,validation_indices = train_test_split(df.index,train_size=0.75,test_size=0.25)
 		r2 = []
+		time_to_predict=[]
 
 		for seed in random.sample(range(1,100),2):
 			tpot = TPOTRegressor(verbosity=2,max_eval_time_mins=0.04,max_time_mins=1,population_size=15,cv=5,random_state=seed,scoring='r2')
 			tpot.fit(df.drop('target',axis=1).loc[training_indices],df.loc[training_indices,'target'])
-			r2.append(tpot.score(df.drop('target',axis=1).loc[validation_indices].values, df.loc[validation_indices, 'target'].values))
+			start_time = time.time()
+			preds = tpot.score(df.drop('target',axis=1).loc[validation_indices].values,df.loc[validation_indices, 'target'].values)
+			time_to_predict.append(time.time() - start_time)
+			r2.append(preds)
 
-		return np.mean(r2)
+		return np.mean(r2),np.mean(time_to_predict)
 
 
-#for oml_data in dataframes:
-	#tpot_score = tpot_benchmarking(oml_data)
-	#print('tpot scores : {}'.format(tpot_score))
+
 
 
 data_ids = {
-				'classification':[1464,40701,1046,1461],
-				'regression':[196,308,537,344]
+				'classification':[1464],#[1464,40701,1046,1461],
+				'regression':[196]#[196,308,537,344]
 			}
 
 
@@ -94,32 +102,34 @@ data_ids = {
 def bench_scoring(data_ids):
 	clf_scores_dict ={}
 	reg_scores_dict = {}
+	timing_dict={}
 
 	for ID in data_ids['classification']:
-		score = tpot_benchmarking(ID,'classification')
+		score,time_to_predict = tpot_benchmarking(ID,'classification')
 		print('score for {} is :{}'.format(ID,score))
 		clf_scores_dict[ID] = score
+		timing_dict[ID]=time_to_predict
 
 	for ID in data_ids['regression']:
-		score = tpot_benchmarking(ID,'regression')
+		score,time_to_predict = tpot_benchmarking(ID,'regression')
 		print('score for {} is :{}'.format(ID,score))
 		reg_scores_dict[ID] = score
+		timing_dict[ID] = time_to_predict
 
 
-	return clf_scores_dict,reg_scores_dict
+
+
+	return clf_scores_dict,reg_scores_dict,timing_dict
 
 
 
-clf_dict,reg_dict = bench_scoring(data_ids)
+clf_dict,reg_dict,time_dict = bench_scoring(data_ids)
 
-#score  =tpot_benchmarking(287,'regression')
+reg = pd.Series(reg_dict,index=reg_dict.keys())
+clf = pd.Series(clf_dict,index=clf_dict.keys())
+timings = pd.Series(time_dict,index=time_dict.keys())
 
-
-#print(score)
-
-
-print('classifiers: {}'.format(clf_dict))
-print('regression:{}'.format(reg_dict))
-
-
+reg.to_csv('TPOT_benchmarking/reg_tpot.csv')
+clf.to_csv('TPOT_benchmarking/clf_tpot.csv')
+timings.to_csv('TPOT_benchmarking/time_to_predict_tpot.csv')
 
